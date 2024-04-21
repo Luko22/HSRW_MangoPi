d73f8700e6f358c7715ac0d8b9b2f448808f16df
= <=>
2024-04-21 11:00:58 +0200

    final changes to map

--- Map/heatmap.py
+++ Map/heatmap.py
@@ -3,61 +3,53 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.ticker as ticker
 import matplotlib.colors as mcolors
-input_file = "accidents_Berlin_2021.csv"
-input_data = pd.read_csv(input_file)
-#input_data = input_data.head(5)
+
+input_file = "Accident_data/accidents19and21_with_quadrants.csv" # The input file
+input_data = pd.read_csv(input_file) # Read the data
 data_len=len(input_data)
-input_data['quadrant']= np.zeros(data_len)
-# berlin latitude range:52.33,52.67
-# berling long range:13.0-13.75
-berlin_latitude_range = (52.336724, 52.675287)
-berlin_long_range = (13.090365, 13.756247)
+input_data['quadrant']= np.zeros(data_len) 
+
+berlin_latitude_range = (52.336724, 52.675287) # Latitude range for Berlin
+berlin_long_range = (13.090365, 13.756247) # Longitude range for Berlin
 multiplication_factor = 100
-lat_range = (int(berlin_latitude_range[0] * multiplication_factor), int(berlin_latitude_range[1] * multiplication_factor))
-lon_range = (int(berlin_long_range[0] * multiplication_factor), int(berlin_long_range[1] * multiplication_factor))
-grid = np.zeros((100,100))
-quadrant_count = pd.read_csv('quadrant_counts.csv', sep=',')
-for index, row in quadrant_count.iterrows():
-    quadrant = row['quadrant']
-    row_idx = int(quadrant // multiplication_factor)
-    print(row_idx)
-    col_idx = int(quadrant % multiplication_factor)
-    print(col_idx)
-    grid[row_idx, col_idx] = row['count']
+lat_range = (float(berlin_latitude_range[0] * multiplication_factor), float(berlin_latitude_range[1] * multiplication_factor)) # Multiply the latitude range by the multiplication factor
+lon_range = (float(berlin_long_range[0] * multiplication_factor), float(berlin_long_range[1] * multiplication_factor)) # Multiply the longitude range by the multiplication factor
+grid = np.zeros((100,100)) # Create a grid of zeros with the size of the multiplication factor
+quadrant_count = pd.read_csv('Accident_data/quadrant_counts_19and21.csv', sep=',') # Read the quadrant counts
+for index, row in quadrant_count.iterrows(): # Iterate over the quadrant counts
+    quadrant = row['quadrant'] # Get the quadrant
+    row_idx = int(quadrant // multiplication_factor) # Get the row index 
+    col_idx = int(quadrant % multiplication_factor) # Get the column index
+    grid[row_idx, col_idx] = row['count'] # Set the grid value to the count
 
-data = np.random.rand(len(lat_range), len(lon_range))
-
+# Create a custom colormap
 cmap = plt.cm.get_cmap('plasma')
 cmap_colors = cmap(np.arange(cmap.N))
 cmap_colors[0] = (0, 0, 0, 0)  # Set the lowest value to be transparent
 custom_cmap = mcolors.ListedColormap(cmap_colors)
 step = 100
-lat=np.linspace(lat_range[0],lat_range[1],step)
-#lat_range=lat_range.flatten()
-lon=np.linspace(lon_range[0],lon_range[1],step)
-map_image = plt.imread('map.jpg')  # Adjust the extent according to your map coordinates
-plt.imshow(map_image, extent=[lon.min(), lon.max(), lat.min(), lat.max()])
+lat=np.linspace(lat_range[0],lat_range[1],step)  # Create a linear space for latitude
+lon=np.linspace(lon_range[0],lon_range[1],step) # Create a linear space for longitude
+map_image = plt.imread('Map/map.jpg')  # Adjust the extent according to your map coordinates
 
+plt.figure(figsize=(20, 10)) # Set the figure size
+plt.imshow(map_image,  extent=[lon_range[0], lon_range[1], lat_range[0], lat_range[1]]) # Plot the map image
 
-print(lon.min())
-print(lon.max())
-print(lat.min())
-print(lat.max())
 
 # Plot grid lines
 for x in lat:
-    plt.plot([lon.min(), lon.max()], [x, x], color='black', alpha=0.5, linewidth=0.5)
+    plt.plot([lon_range[0], lon_range[1]], [x, x], color='none', alpha=0.5, linewidth=0.5)
 
 for y in lon:
-    plt.plot([y, y], [lat.min(), lat.max()], color='black', alpha=0.5, linewidth=0.5)
+    plt.plot([y, y], [lat_range[0], lat_range[1]], color='none', alpha=0.5, linewidth=0.5)
 
 # Add grid labels
-plt.gca().xaxis.set_major_locator(ticker.MultipleLocator(20))  # Adjust the tick frequency as needed
-plt.gca().yaxis.set_major_locator(ticker.MultipleLocator(20))  # Adjust the tick frequency as needed
+plt.gca().xaxis.set_major_locator(ticker.MultipleLocator(100))  # Adjust the tick frequency as needed
+plt.gca().yaxis.set_major_locator(ticker.MultipleLocator(100))  # Adjust the tick frequency as needed
 
-plt.imshow(grid, cmap=custom_cmap, alpha=0.4, extent=[lon_range[0], lon_range[1], lat_range[0], lat_range[1]])
+plt.imshow(grid, cmap=custom_cmap, alpha=0.4, extent=[lon_range[0], lon_range[1], lat_range[0], lat_range[1]]) # Plot the heatmap
 
-#plt.colorbar(label='Accident Rate')
+plt.colorbar(label='Accident Rate')
 plt.xlabel('Longitude')
 plt.ylabel('Latitude')
 plt.title('Accident Heatmap')
--- Map/map.py
+++ Map/map.py
@@ -2,26 +2,18 @@ import osmnx as ox
 import matplotlib.pyplot as plt
 ox.settings.log_console = True
 # Define the bounding box coordinates (latitude and longitude) for your area of interest
-north, south, east, west = 52.675287, 52.336724, 13.090365, 13.756247
-#plt.figure(figsize=(15, 15))
-# Download OSM data for the area
-G = ox.graph_from_bbox(north, south, east, west, network_type='drive')
+def createmap():
+    north, south, east, west = 52.675287, 52.336724, 13.090365, 13.756247 #define bounds of map area
 
-#ec = ['b' if data.get('highway') is not None else 'w' for u, v, key, data in G.edges(keys=True, data=True)]
+    G = ox.graph_from_bbox(north, south, east, west, network_type='drive') #retrieve map of area from OpenStreetMap
 
+    #plot the map
+    fig, ax = ox.plot_graph(G, bgcolor='w', node_color='none', node_edgecolor='none', edge_color='k', edge_linewidth=0.5,show=False, close=False, bbox=(south, north, west, east))
 
-# Plot the map
-#fig, ax = ox.plot_graph(G, node_color='none', node_edgecolor='none', node_size=30, edge_linewidth=0.5, edge_color='k', bgcolor='w')
-#plt.figure(figsize=(15, 15))
-fig, ax = ox.plot_graph(G, bgcolor='w', node_color='none', node_edgecolor='none', edge_color='k', edge_linewidth=0.5,show=False, close=False, bbox=(south, north, west, east))
-# Add grid lines
-#ax.grid(True)
-plt.gcf().set_size_inches(10, 10)
-# Add labels and title
-#plt.xlabel('Longitude')
-#plt.ylabel('Latitude')
-#plt.title('Map of the Area')
-ax.set_facecolor('black')
-plt.axis('off')
-# Show the plot
-plt.show()
+    plt.gcf().set_size_inches(10, 10)
+    ax.set_facecolor('black')
+    plt.axis('off')
+    # Show the plot
+    plt.show()
+
+createmap()
